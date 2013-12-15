0ee399a6b7e1ddc91ab8b4038a70785869c0184c
Sam Soffes <sam@soff.es>
2013-12-14 23:26:20 -0500

    First pass

--- lib/unmarkdown/parser.rb
+++ lib/unmarkdown/parser.rb
@@ -1,11 +1,92 @@
+require 'nokogiri'
+
 module Unmarkdown
   class Parser
-    def initialize(html)
+    def initialize(html, options = {})
       @html = html
+      @options = options
     end
 
     def parse
-      @html
+      # Setup document
+      doc = Nokogiri::HTML(@html)
+      doc.encoding = 'UTF-8'
+
+      # Parse the root node recursively
+      root_node = doc.xpath('//body')
+      markdown = parse_nodes(root_node.children)
+
+      # TODO: Optionally look for auto links
+
+      # Strip whitespace
+      markdown.strip.gsub(/\n{2}+/, "\n\n")
+
+      # TODO: Strip trailing whitespace
+    end
+
+    private
+
+    # Parse the children of a node
+    def parse_nodes(nodes)
+      output = ''
+
+      # Short-circuit if it's empty
+      return output if !nodes || nodes.empty?
+
+      # Loop through nodes
+      nodes.each do |node|
+        case node.name
+        when 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'
+          hashes = ''
+          node.name.match(/\Ah(\d)\Z/)[1].to_i.times { hashes << '#' }
+          output << "#{hashes} #{parse_content(node)}\n\n"
+        when 'blockquote'
+          parse_content(node).split("\n").each do |line|
+            output << "> #{line}\n"
+          end
+        # when 'ul', 'ol'
+        when 'pre'
+          parse_content(node).split("\n").each do |line|
+            output << "    #{line}\n"
+          end
+        when 'hr'
+          output << "---\n\n"
+        when 'a'
+          output << "[#{parse_content(node)}](#{node['href'] + build_title(node)})"
+        when 'i', 'em'
+          output << "*#{parse_content(node)}*"
+        when 'b', 'strong'
+          output << "**#{parse_content(node)}**"
+        when 'u'
+          output << "_#{parse_content(node)}_"
+        when 'mark'
+          output << "==#{parse_content(node)}=="
+        when 'code'
+          output << "`#{parse_content(node)}`"
+        when 'img'
+          output << "![#{node['alt']}](#{node['src'] + build_title(node)})"
+        else
+          # If it's an supported node or a node that just contains text, just get
+          # its content
+          output << parse_content(node)
+        end
+      end
+
+      output
+    end
+
+    # Get the content from a node
+    def parse_content(node)
+      content = if node.children.empty?
+        node.content
+      else
+        parse_nodes(node.children)
+      end
+    end
+
+    # Build the title for links or images
+    def build_title(node)
+      node['title'] ? %Q{ "#{node['title']}"} : ''
     end
   end
 end
